eb66a7b86857e57071aef65daee85f1851dacba4
whliaf <whliaf@gmail.com>
2024-02-28 19:26:43 +0800

    First commit

---
+++ src/app/left-menu/left-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './left-menu.component.html',
+  styleUrl: './left-menu.component.css'
+})
+export class LeftMenuComponent {
+
+}
